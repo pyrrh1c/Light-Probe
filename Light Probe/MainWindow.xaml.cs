62a7b9ab450c1eb9a270c45b07cf7d55e73671a5
Pyrrh1c <pyrrh1c@protonmail.com>
2018-11-01 08:02:01 -0400

    Just bringing what I have so far into source control. Going to start cleaning things up from here...

--- Light Probe/MainWindow.xaml.cs
+++ Light Probe/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace Light_Probe
 {
@@ -20,9 +21,100 @@ namespace Light_Probe
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] ports = SerialPort.GetPortNames();
+        SerialPort port = new SerialPort();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach (string port in ports)
+            {
+                try
+                {
+                    SerialPort testPort = new SerialPort(port, 9600);
+                    testPort.Open();
+                    CB_Ports.Items.Add(port);
+                    testPort.Close();
+                    testPort = null;
+                }
+                catch
+                {
+                }
+            }
         }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string message = "";
+            this.Dispatcher.Invoke(() =>
+            {
+                try
+                {
+                    message = port.ReadLine();
+                    TB_LastMessage.Text = message;
+                    PB_ULSensor.Value = int.Parse(message.Substring(3, 3));
+                    PB_URSensor.Value = int.Parse(message.Substring(10, 3));
+                    PB_LLSensor.Value = int.Parse(message.Substring(17, 3));
+                    PB_LRSensor.Value = int.Parse(message.Substring(24, 3));
+                    //Set the overall average
+                    PB_SensorAverage.Value = (PB_ULSensor.Value +
+                        PB_URSensor.Value +
+                        PB_LLSensor.Value +
+                        PB_LRSensor.Value) / 4;
+
+                    //Set the horizontal balance
+                    PB_HorizontalBalance.Value = 50 + (PB_URSensor.Value - PB_ULSensor.Value);
+                    //Set the Vertical balance
+                    PB_VerticalABalance.Value = 50 + (PB_LRSensor.Value - PB_LLSensor.Value);
+                }
+                catch
+                {
+                    TB_LastMessage.Text = "Message Error";
+                }
+            });
+        }
+
+        private void CB_Ports_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            port.Close();
+            port = null;
+            port = new SerialPort(CB_Ports.SelectedValue.ToString(), 9600);
+            try
+            {
+                port.Open();
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            }
+            catch
+            {
+                TB_LastMessage.Text = "Port Error";
+            }
+        }
+
+        private void UpdateValue(ProgressBar targetBar, double targetValue)
+        {
+            targetBar.Value = targetValue;
+
+            if (targetValue < 25)
+            {
+                targetBar.Foreground = Brushes.Red;
+            }
+            else if ((targetValue >= 25) && (targetValue < 75))
+            {
+                targetBar.Foreground = Brushes.Yellow;
+            }
+            else
+            {
+                targetBar.Foreground = Brushes.Green;
+            }
+
+        }
+    }
+}
+public static class ExtensionMethods
+{
+    public static decimal Map(this decimal value, decimal fromSource, decimal toSource, decimal fromTarget, decimal toTarget)
+    {
+        return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
     }
 }
